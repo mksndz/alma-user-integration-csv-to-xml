cac96b3dd53efe31a00a0c3a4fd583f4ccf7ae27
Mike Kanning <mikekanning@gmail.com>
2016-09-01 15:11:39 -0400

    add method details and basic validation

--- lib/objects/user.rb
+++ lib/objects/user.rb
@@ -3,6 +3,7 @@ require 'csv'
 class User
 
   USER_ATTRIBUTES = %w(primary_id first_name middle_name last_name gender user_group campus_code status address_line_1 address_line_2 address_city address_state_province address_postal_code address_country email phone)
+  MAXIMUM_STRING_VALUE_LENGTH = 255
 
   attr_reader *USER_ATTRIBUTES
 
@@ -20,68 +21,127 @@ class User
 
   end
 
+  # ALMA PRIMARY ID
+  # type:         string
+  # max_length:   255
   def primary_id=(v)
-    @primary_id = v
+    @primary_id = alma_string v
   end
-  
+
+  # FIRST NAME
+  # type:         string
+  # max_length:   255
   def first_name=(v)
-    @first_name = v
+    @first_name = alma_string v
   end
-  
+
+  # MIDDLE NAME
+  # type:         string
+  # max_length:   255
   def middle_name=(v)
-    @middle_name = v
+    @middle_name = alma_string v
   end
-  
+
+  # LAST NAME
+  # type:         string
+  # max_length:   255
   def last_name=(v)
-    @last_name = v
+    @last_name = alma_string v
   end
-  
+
+  # GENDER
+  # type:         string
+  # max_length:   255
+  # table:        Genders
   def gender=(v)
-    @gender = v
+    @gender = alma_string v
   end
-  
+
+  # USER_GROUP
+  # type:         string
+  # max_length:   255
+  # table:        UserGroups
+  # override:     false
   def user_group=(v)
-    @user_group = v
+    @user_group = alma_string v
   end
-  
+
+  # CAMPUS CODE
+  # type:         string
+  # max_length:   50
+  # override:     false
   def campus_code=(v)
-    @campus_code = v
+    @campus_code = alma_string v, 50
   end
-  
+
+  # STATUS
+  # type:         string
+  # max_length:   255
+  # table:        ContentStructureStatus
   def status=(v)
-    @status = v
+    @status = alma_string v
   end
-  
+
+  # ADDRESS LINE 1
+  # type:         string
+  # max_length:   255
   def address_line_1=(v)
-    @address_line_1 = v
+    @address_line_1 = alma_string v
   end
-  
+
+  # ADDRESS LINE 2
+  # type:         string
+  # max_length:   255
   def address_line_2=(v)
-    @address_line_2 = v
+    @address_line_2 = alma_string v
   end
-  
+
+  # ADDRESS CITY
+  # type:         string
+  # max_length:   255
   def address_city=(v)
-    @address_city = v
+    @address_city = alma_string v
   end
-  
+
+  # ADDRESS STATE/PROVINCE
+  # type:         string
+  # max_length:   255
   def address_state_province=(v)
-    @address_state_province = v
+    @address_state_province = alma_string v
   end
-  
+
+  # ADDRESS POSTAL CODE
+  # type:         string
+  # max_length:   255
   def address_postal_code=(v)
-    @address_postal_code = v
+    @address_postal_code = alma_string v
   end
-  
+
+  # ADDRESS COUNTRY
+  # type:         string
+  # max_length:   255
   def address_country=(v)
-    @address_country = v
+    @address_country = alma_string v
   end
-  
+
+  # ADDRESS EMAIL
+  # type:         string
+  # max_length:   255
   def email=(v)
-    @email = v
+    @email = alma_string v
   end
-  
+
+  # ADDRESS PHONE
+  # type:         string
+  # max_length:   255
   def phone=(v)
-    @phone = v
+    @phone = alma_string v
+  end
+
+  private
+
+  def alma_string(str, size = MAXIMUM_STRING_VALUE_LENGTH)
+    str.truncate size
   end
 
 end
